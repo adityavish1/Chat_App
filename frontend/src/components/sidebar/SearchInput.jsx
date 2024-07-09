30171b23f2d18cdba6e1c82c6b57cd3b05d93663
Aditya Vishwakarma <ad805257@gmail.com>
2024-07-10 02:31:37 +0530

    Update SearchInput.jsx

--- frontend/src/components/sidebar/SearchInput.jsx
+++ frontend/src/components/sidebar/SearchInput.jsx
@@ -24,7 +24,7 @@ const SearchInput = () => {
 		} else toast.error("No such user found!");
 	};
 	return (
-		<form onSubmit={handleSubmit} className='flex items-center gap-2'>
+		<form onSubmit={handleSubmit} className='flex items-center gap-2 sticky top-0'>
 			<input
 				type='text'
 				placeholder='Search…'
