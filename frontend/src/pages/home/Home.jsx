a2a7ce8441e26b45e19a6111bab8140a8c896cfa
Aditya Vishwakarma <ad805257@gmail.com>
2024-07-10 02:13:34 +0530

    Update Home.jsx

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -4,8 +4,13 @@ import Sidebar from "../../components/sidebar/Sidebar";
 const Home = () => {
 	return (
 		<div className='flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0 flex-col sm:flex-row'>
-			<Sidebar className=' h-1/2'/>
-			<MessageContainer className=' h-1/2'/>
+			<div className=' h-1/2'>
+			<Sidebar/>
+			</div>
+			<div className=' h-1/2'>
+			<MessageContainer />
+			</div>
+			
 		</div>
 	);
 };
