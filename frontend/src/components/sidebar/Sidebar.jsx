15a087404a3d1b8f19159fffcbf6923d91de5609
Aditya Vishwakarma <ad805257@gmail.com>
2024-07-10 02:26:47 +0530

    Update Sidebar.jsx

--- frontend/src/components/sidebar/Sidebar.jsx
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -5,7 +5,10 @@ import SearchInput from "./SearchInput";
 const Sidebar = () => {
 	return (
 		<div className='border-r border-slate-500 p-4 flex flex-col'>
-			<SearchInput className='sticky top-0'/>
+			<div className='sticky top-0'>
+			<SearchInput />
+			</div>
+			
 			<div className='divider px-3'></div>
 			<Conversations />
 			<LogoutButton />
