129c1c41f72ffd34f3b6f08303787b3243ae7f84
Aditya Vishwakarma <ad805257@gmail.com>
2024-07-10 03:30:32 +0530

    Update MessageContainer.jsx

--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -15,7 +15,7 @@ const MessageContainer = () => {
 
 	return (
 		
-		<div className='md:min-w-[450px] flex flex-col overflow-hidden'>
+		<div className='md:min-w-[450px] flex flex-col'>
 			{!selectedConversation ? (
 				<NoChatSelected />
 			) : (
