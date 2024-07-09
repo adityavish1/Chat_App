f0b8564be0e4389f9d73594b62a2eed443ab206e
Aditya Vishwakarma <ad805257@gmail.com>
2024-07-10 02:52:02 +0530

    Update Home.jsx

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -4,10 +4,10 @@ import Sidebar from "../../components/sidebar/Sidebar";
 const Home = () => {
 	return (
 		<div className='flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0 flex-col sm:flex-row'>
-			<div className='h-1/3 sm:h-full  overflow-scroll'>
+			<div className='h-1/3 sm:h-full'>
 			<Sidebar/>
 			</div>
-			<div className='h-1/3 sm:h-full overflow-scroll'>
+			<div className='h-1/3 sm:h-full'>
 			<MessageContainer />
 			</div>
 			
